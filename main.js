8eed24b10808d6c396903351c2291c08f6ee7434
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-10-14 15:07:27 -0400

    passing initial layout

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
-document.querySelector("#submitbtn").addEventListener("submit", (event) => {
-  event.preventDefault();
-  fetch("https://ghibliapi.herokuapp.com/films")
-    .then((res) => {
-      return res.json();
-    })
-    .then((films) => {
-      console.log(films);
+fetch("https://ghibliapi.herokuapp.com/films")
+  .then((res) => res.json())
+  .then((films) => {
+    console.log(films);
 
-      let dropDown = document.querySelector("#dropdown");
+    const dropDown = document.querySelector("#dropdown");
 
-      films.forEach((film) => {
-        const option = document.createElement("option");
-        option.setAttribute("value", film.name);
-        option.textContent = film.name;
-        dropDown.append(option);
+    for (let film of films) {
+      const option = document.createElement("option");
+      option.textContent = film.title;
+      option.setAttribute("value", film.title);
+      dropDown.append(option);
+    }
+
+    let displayInfo = document.querySelector("#display-info");
+    //add li's
+    dropDown.addEventListener("change", () => {
+      for (let film of films) {
+        let releasedDate = film.released_date;
+        let filmDescription = film.description;
+
+        if (dropDown.value === film.title) {
+          console.log(film.title);
+          displayInfo.innerHTML = `<h3>${film.title}</h3>
+            <p id="releasedDate">${releasedDate}</p>
+            <p id="film-description">${filmDescription}</p>`;
+        }
+      }
+
+      //Review section
+      let ul = document.querySelector("#reviewlist");
+
+      document.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        const li = document.createElement("li");
+        li.innerHTML = `<strong>${filmheader.value}</strong>`;
+        //   li.textContent = `${event.target.input.value}`;
+        ul.append(li);
       });
     });
-});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
